b637517edb8e54bb79fa9e9267a78e2b99ae646b
kevin-j-miller <kjmd10@gmail.com>
2022-05-21 07:10:17 +0100

    Modify and improve rpe figure

--- figure_scripts/fig4-sN_rpe_scatterplot.m
+++ figure_scripts/fig4-sN_rpe_scatterplot.m
@@ -1,75 +1,123 @@
+%% Load necessary data
 ephys_regression_results = load(fullfile(files_path, 'postprocessed_data', 'ephys_regression_results'));
 nUnits = length(ephys_regression_results.weights(1,:))
-
-% Which timebin should we look at? Bins are 200ms long.
-timebin_to_consider = 0.6; % In seconds, from the time of reward.
-ind_timebin = find(abs(ephys_regression_results.bin_mids_by_lock{2} - timebin_to_consider) < 0.01)
+ps = load(fullfile(files_path, 'postprocessed_data', 'unit_p_values'))
 
 
-reward_time_weights_cell = ephys_regression_results.weights(2,:);
+lock = 2; % Second time lock is locked to reward delivery
 
+
+reward_time_weights_cell = ephys_regression_results.weights(lock, :);
+
+lock_weights = [];
 for unit_i = 1:length(reward_time_weights_cell)
-lock_weights(unit_i,:,:) = reward_time_weights_cell{unit_i};
+    lock_weights(unit_i,:,:) = reward_time_weights_cell{unit_i};
 end
 
 
 
-%% Sanity check: Plot the median abs weight for each regressor. 
+%% Sanity check: Plot the median abs weight for each regressor.
 % Check that the timecourses look like what you expect
-abs_weight_timecourse = squeeze(median(abs(lock_weights)))
+abs_weight_timecourse = squeeze(median(abs(lock_weights)));
 
 figure; hold on
 for reg_i = 1:11
-plot(ephys_regression_results.bin_mids_by_lock{2}, ...
-    abs_weight_timecourse(:, reg_i) - reg_i)
+    plot(ephys_regression_results.bin_mids_by_lock{lock}, ...
+        abs_weight_timecourse(:, reg_i) - reg_i)
 end
 
 % Index of the reward and outcome value regressors in the list of
-% regressors (see construct_regressors_ephys) plus one because an offset
-% tregressor gets added later (see iti_regression_copd)
+% regressors (see construct_regressors_ephys)
+ind_reward = 3;
+ind_outcome_value = 8; %8;
 
-% Check these against plot above
-ind_reward = 4; 
-ind_outcome_value = 9; 
 
-bin_size = 0.2; % 200ms timebins
+%% Pick a time bin
+
+% Which timebin should we look at? Bins are 200ms long.
+timebin_to_consider = 0.4; % In seconds, from the time of reward.
+ind_timebin = find(abs(ephys_regression_results.bin_mids_by_lock{lock} - timebin_to_consider) < 0.01)
+
 
 %% Make the scatterplot
+
+colors = colors_task;
 
 figure; hold on
 line([0,0], [-100, 100], 'color', 'k')
 line([-100, 100], [0, 0], 'color', 'k')
 line([-100, 100], [100, -100], 'linestyle', '--', 'color', [0.5, 0.5, 0.5])
 
-% Weights / bin_size to put them in units of sp/s
-xs = lock_weights(:, ind_timebin, ind_reward) / bin_size;
-ys =     lock_weights(:, ind_timebin, ind_outcome_value) / bin_size;
+% Add one to account for the offset regressor (see iti_regression_copd)
+xs = lock_weights(:, ind_timebin, ind_reward + 1);
+ys = lock_weights(:, ind_timebin, ind_outcome_value + 1);
 
-scatter(xs, ys, ...
+
+% Identify significant reward and value units
+alpha = 0.01;
+ps_reward = ps.p_timecourse{lock}(:, ind_reward, ind_timebin);
+ps_value = ps.p_timecourse{lock}(:, ind_outcome_value, ind_timebin);
+
+reward_units = ps_reward < alpha;
+value_units = ps_value < alpha;
+
+reward_only = reward_units & ~value_units;
+value_only = ~reward_units & value_units;
+rpe = reward_units & value_units & (sign(xs) ~= sign(ys));
+upQ = reward_units & value_units & (sign(xs) == sign(ys));
+neither = ~reward_units & ~value_units;
+
+
+dot_colors = zeros(nUnits,3);
+dot_colors(neither, :) = repmat([0.7, 0.7, 0.7], [sum(neither),1]);
+dot_colors(reward_only, :) = repmat(colors(3,:), [sum(reward_only),1]);
+dot_colors(value_only, :) = repmat(rew_color, [sum(value_only),1]);
+dot_colors(rpe | upQ, :) = repmat(task_green, [sum(rpe | upQ),1]);
+
+% Plot the "neither"s underneath all others
+scatter(xs(neither), ys(neither), 50, ...
+    dot_colors(neither,:),...
     '.');
 
-xlabel({'Regression Weight:', 'Reward (sp/s)'})
-ylabel({'Regression Weight:', 'Outcome Value (sp/s)'})
+scatter(xs(~neither), ys(~neither), 50, ...
+    dot_colors(~neither,:),...
+    '.');
 
-xlim([-20, 20])
-ylim([-20, 20])
+% Label the plot with text
+text(-4.9, -3,   ['Reward Only:    ' num2str(sum(reward_only))], 'color', colors(3,:))
+text(-4.9, -3.5, ['Value Only:     ' num2str(sum(value_only))], 'color', rew_color)
+text(-4.9, -4,   ['Reward & Value: ' num2str(sum(rpe | upQ))], 'color', task_green)
+
+xlabel({'Regression Weight:', 'Reward'})
+ylabel({'Regression Weight:', 'Outcome Value'})
+
+xlim([-5, 5])
+ylim([-5, 5])
 
 axis square
 set(gca,'fontsize', 20)
 
 %% Do some stats
-reward_pos = mean(xs > 0)
-value_pos = mean(ys > 0)
+% Among significant units
+disp([num2str(sum(reward_units & value_units)) ' units correlate with both value and reward'])
+disp(['of those, ' num2str(sum(rpe)) ' have opposite sign for the two. This is %' num2str(100*sum(rpe) / sum(reward_units & value_units))])
+disp(['the p-value (binomial test) for this is ' ...
+num2str(1 - binocdf(sum(rpe), sum(reward_units & value_units), 0.5))]);
 
-rpe_null = reward_pos * (1 - value_pos) + (1 - reward_pos) * value_pos
-rpe_true_sum = sum(sign(xs) ~= sign(ys))
-rpe_true_mean = mean(sign(xs) ~= sign(ys))
+% Among all units
+rpe_sum = sum(sign(xs) ~= sign(ys))
+rpe_mean = mean(sign(xs) ~= sign(ys))
 
-1 - binocdf(rpe_true_sum, nUnits, 0.5)
+disp(['Considering all units, ' ...
+    num2str(rpe_sum) ...
+    ' have opposite sign for value and reward.'])
+    disp(['This is %' ...
+    num2str(100*rpe_mean), ...
+    ' of the total'])
+disp(['the p-value (binomial test) for this is ' ...
+num2str(1 - binocdf(rpe_sum, nUnits, 0.5))]);
 
-bottom_left =  sum(xs < 0 & ys < 0)
-top_left =     sum(xs < 0 & ys > 0)
-bottom_right = sum(xs > 0 & ys < 0)
-top_right =    sum(xs > 0 & ys > 0)
+
+
 
 
